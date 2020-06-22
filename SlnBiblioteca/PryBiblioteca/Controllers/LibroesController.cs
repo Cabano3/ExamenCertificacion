841b0d5fb4e41747f90b6c848aefba0f59e03eb1
cristian baño <cristian1898abr@outlook.es>
2020-06-22 15:09:29 -0500

    Exámen

---
+++ SlnBiblioteca/PryBiblioteca/Controllers/LibroesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEUBiblioteca;
+using BEUBiblioteca.Transactions;
+
+namespace PryBiblioteca.Controllers
+{
+    public class LibroesController : Controller
+    {
+        // GET: Libroes
+        public ActionResult Index()
+        {
+            return View(LibroBLL.List());
+        }
+
+        // GET: Libroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = LibroBLL.Get(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // GET: Libroes/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre");
+            return View();
+        }
+
+        // POST: Libroes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idLibro,titulo,autor,isbn,fechapublicacion,numeroejemplares,idCategoria")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                LibroBLL.Create(libro);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", libro.idCategoria);
+            return View(libro);
+        }
+
+        // GET: Libroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = LibroBLL.Get(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", libro.idCategoria);
+            return View(libro);
+        }
+
+        // POST: Libroes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idLibro,titulo,autor,isbn,fechapublicacion,numeroejemplares,idCategoria")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                LibroBLL.Update(libro);
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", libro.idCategoria);
+            return View(libro);
+        }
+
+        // GET: Libroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = LibroBLL.Get(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: Libroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LibroBLL.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SlnBiblioteca/PryBiblioteca/Controllers/VideosController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEUBiblioteca;
+using BEUBiblioteca.Transactions;
+
+namespace PryBiblioteca.Controllers
+{
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return View(VideoBLL.List());
+        }
+
+        // GET: Videos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = VideoBLL.Get(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        // GET: Videos/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre");
+            return View();
+        }
+
+        // POST: Videos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idVideo,titulo,fechapublicacion,formato,duracion,idCategoria")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                VideoBLL.Create(video);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", video.idCategoria);
+            return View(video);
+        }
+
+        // GET: Videos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = VideoBLL.Get(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", video.idCategoria);
+            return View(video);
+        }
+
+        // POST: Videos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idVideo,titulo,fechapublicacion,formato,duracion,idCategoria")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                VideoBLL.Update(video);
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCategoria = new SelectList(CategoriaBLL.List(), "idCategoria", "nombre", video.idCategoria);
+            return View(video);
+        }
+
+        // GET: Videos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = VideoBLL.Get(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        // POST: Videos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VideoBLL.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
